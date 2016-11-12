b5a857d6ad253ebe7aedaab5b563eb2f48993bfe
Anson <anson32330200@gmail.com>
2016-11-11 16:39:29 -0800

    Finished

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,6 +4,7 @@
 #
 
 import psycopg2
+import bleach
 
 
 def connect():
@@ -13,14 +14,39 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute('''
+    DELETE FROM matches;
+    ''')
+    c.execute('''
+    UPDATE players SET matches = 0, wins = 0;
+    ''')
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute('''
+    DELETE FROM players;
+    ''')
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute('''
+    SELECT COUNT(*) FROM players;
+    ''')
+    num = c.fetchall()[0][0]
+    c.close()
+    return num
 
 
 def registerPlayer(name):
@@ -32,6 +58,14 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    c = conn.cursor()
+    data = (bleach.clean(name),)
+    c.execute('''
+    INSERT INTO players (name, wins, matches) VALUES (%s, 0, 0);
+    ''', data)
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,6 +81,15 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute('''
+    SELECT id, name, wins, matches FROM players
+    ORDER BY wins DESC;
+    ''')
+    players = c.fetchall()
+    conn.close()
+    return players
 
 
 def reportMatch(winner, loser):
@@ -56,10 +99,43 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    def updateWinner():
+        c.execute('''
+        SELECT wins, matches FROM players
+        WHERE id = %s;
+        ''', (winner,))
+        data = c.fetchone()
+        wins, matches = data[0], data[1]
+
+        c.execute('''
+        UPDATE players SET wins = %s, matches = %s where id = %s;
+        ''', (wins + 1, matches + 1, winner,))
+
+    def updateLoser():
+        c.execute('''
+        SELECT matches FROM players
+        WHERE id = %s;
+        ''', (loser,))
+        matches = c.fetchone()[0]
+
+        c.execute('''
+        UPDATE players SET matches = %s where id = %s;
+        ''', (matches + 1, loser,))
+
+    conn = connect()
+    c = conn.cursor()
+    c.execute('''
+    INSERT INTO matches (winner_id, loser_id) VALUES (%s, %s);
+    ''', (winner, loser,))
+    updateWinner()
+    updateLoser()
+    conn.commit()
+    conn.close()
  
  
 def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
+    """
+    Returns a list of pairs of players for the next round of a match.
   
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
@@ -73,5 +149,13 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    res = []
+    i, n, players = 0, countPlayers(), playerStandings()
+    while i < n:
+        p1, p2 = players[i], players[i + 1]
+        res.append((p1[0], p1[1], p2[0], p2[1]))
+        i += 2
+    return res
+
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,21 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE players (
+    id serial PRIMARY KEY,
+    name text,
+    wins integer,
+    matches integer
+);
+
+-- win = TRUE if player 1 wins, else FALSE
+CREATE TABLE matches (
+    winner_id serial REFERENCES players (id),
+    loser_id serial REFERENCES players (id)
+);
 
